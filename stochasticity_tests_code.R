6a7b6c81a252da7aa6cff7b897f029f02e0f745c
Tanya Rogers <tanya.l.rogers@gmail.com>
2022-10-21 13:58:29 -0700

    just use rel file paths

--- stochasticity_tests_code.R
+++ stochasticity_tests_code.R
@@ -2,14 +2,15 @@
 
 # Load libraries
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load(here, rdist, tseriesChaos, fractal)
+pacman::p_load(rdist, tseriesChaos, fractal)
 #The fractal package has been archived now and so you may have to download
 #it manually from the CRAN archive
 
+source('functions/embed_udf.R')
+source('functions/predict_np_udf.R')
+
 # List all time series
-here::i_am("stochasticity_tests_code.R")
-input_dir <- setwd(here("data"))
-input_files <- list.files(pattern = "*.csv")
+input_files <- list.files(path = "data/", pattern = "*.csv")
 
 # Loop and run tests on each
 for(i in seq_along(input_files)){
@@ -23,8 +24,6 @@ for(i in seq_along(input_files)){
   print(input_files[i])
   
   #Step 2: Embed time series (x)
-  dump("embed_udf", file=here::here('embed_udf.R'))
-  source(here::here('embed_udf.R'))
   results.embed_udf<-embed_udf(x)
   d<-results.embed_udf[[1]]
   m<-results.embed_udf[[2]]
@@ -33,7 +32,6 @@ for(i in seq_along(input_files)){
   dist<-rdist(Mx) #distance matrix, rdist(fields)
   
   #Step 3: Nonlinear prediction skill
-  source(here::here('predict_np_udf.R'))
   results.np<-predict_np_udf(Mx,frac.learn=0.5)
   nse.x<-results.np[[1]]
   print("nonlinear prediction skill:")
@@ -42,9 +40,9 @@ for(i in seq_along(input_files)){
   #Step 4: Kaplan's delta-epsilon test for determinism
   kap.test <- determinism(x) # delta-epsilon test
   #print(kap.test) # summary of the analysis
-  file_name = paste("Kaplan_test_GPDD_", tools::file_path_sans_ext(input_files[i]), 
+  file_name = paste("result_plots/Kaplan_test_GPDD_", tools::file_path_sans_ext(input_files[i]), 
                     ".tiff", sep="") # name plots
-  tiff(file_name, units="in", width=5, height=5, res=300)
+  tiff(file_name, units="in", width=5, height=5, res=300, compression = "lzw")
   print(plot(kap.test)) # plots E-statistics of  original series and surrogates
   dev.off()
 }
